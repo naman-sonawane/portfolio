3e9543f931b1999f6a88a0cbff3c47ae8f6a3e39
Naman Sonawane <sonawane.naman14@gmail.com>
2024-08-15 15:19:26 -0400

    Work page WIP

--- components/ui/certspopup.tsx
+++ components/ui/certspopup.tsx
@@ -13,20 +13,24 @@ const Popup: React.FC = () => {
   const badgeData = [
     {
       src: "https://media.licdn.com/dms/image/C4E12AQH-e2WPGP0YXQ/article-cover_image-shrink_600_2000/0/1649839178239?e=2147483647&v=beta&t=5uPmguARjr_MMdIw3QyUjtH9RbZQ8vTKuezgHQ_SFTc",
-      url: "https://www.credly.com/badges/abc123",
+      url: "https://www.credly.com/badges/594e6cd4-a640-4e23-9f89-ddd2ef1f664b/linked_in_profile",
     },
     {
       src: "https://images.squarespace-cdn.com/content/v1/624f533702b64e5c9c78e131/1678942955789-QY2AY83IJ497E181JUDZ/AI-900+Badge.png",
-      url: "https://www.credly.com/badges/def456",
+      url: "https://www.credly.com/badges/b00b0667-d014-41f6-8bb5-47985ecdc6e9/linked_in_profile",
     },
     {
       src: "https://crackcerts.com/uploads/course_image/az-900_1667391877.webp",
-      url: "https://www.credly.com/badges/ghi789",
+      url: "https://www.credly.com/badges/965700dd-65b1-4282-8fc4-a539b84d7a0e/linked_in_profile",
     },
     {
       src: "https://www.databricks.com/sites/default/files/2021/12/lakehouse-fundamentals.png",
-      url: "https://www.credly.com/badges/jkl012",
+      url: "https://credentials.databricks.com/82633c06-a728-40c9-95d3-6fee7d3cf855",
     },
+    {
+        src: "https://www.databricks.com/sites/default/files/2021/12/lakehouse-fundamentals.png",
+        url: "https://credentials.databricks.com/520eff16-3773-49ca-bb56-02f8d08dd69d",
+    }
   ];
 
   return (
@@ -42,20 +46,20 @@ const Popup: React.FC = () => {
         </ModalTrigger>
         <ModalBody>
           <ModalContent>
-          <h4 className="text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold text-center mb-4">
+            <h4 className="text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold text-center mb-4">
               My Achievements
             </h4>
             <p className="text-sm md:text-base text-neutral-500 dark:text-neutral-400 text-center mb-6">
               Here are some of the certifications in Azure Cloud and AI I have earned.<br/> Click on them to view details.
             </p>
-            <div className="flex flex-wrap gap-4 justify-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
               {badgeData.map((badge, index) => (
                 <a
                   key={index}
                   href={badge.url}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="w-1/2 md:w-1/4 lg:w-1/6 flex justify-center items-center"
+                  className="flex justify-center items-center"
                 >
                   <img
                     src={badge.src}
