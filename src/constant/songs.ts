718c933e1766f6f7078e37ad10057e164658b71f
naman-sonawane <sonawane.naman14@gmail.com>
2026-04-30 15:40:36 -0400

    reorder

--- src/constant/songs.ts
+++ src/constant/songs.ts
@@ -7,34 +7,10 @@ export interface Song {
 
 export const SONGS: Song[] = [
   {
-    title: "Butterflies",
-    artist: "MAX, Ali Gatie",
-    src: "/songs/butterflies.mp3",
-    cover: "/covers/butterflies.jpg",
-  },
-  {
-    title: "endlessly",
-    artist: "Bixby",
-    src: "/songs/endlessly.mp3",
-    cover: "/covers/endlessly.jpg",
-  },
-  {
     title: "Moonlight",
     artist: "Chase Atlantic",
     src: "/songs/moonlight.mp3",
     cover: "/covers/moonlight.jpg",
-  },
-  {
-    title: "Out of Time",
-    artist: "The Weeknd",
-    src: "/songs/outoftime.mp3",
-    cover: "/covers/outoftime.jpg",
-  },
-  {
-    title: "peach eyes",
-    artist: "wave to earth",
-    src: "/songs/peacheyes.mp3",
-    cover: "/covers/peacheyes.png",
   },
   {
     title: "Perfect",
@@ -43,10 +19,33 @@ export const SONGS: Song[] = [
     cover: "/covers/perfect.jpg",
   },
   {
+    title: "BUTTERFLIES",
+    artist: "MAX, Ali Gatie",
+    src: "/songs/butterflies.mp3",
+    cover: "/covers/butterflies.jpg",
+  },
+  {
     title: "Talk",
     artist: "Why Don't We",
     src: "/songs/talk.mp3",
     cover: "/covers/talk.jpg",
   },
+  {
+    title: "peach eyes",
+    artist: "wave to earth",
+    src: "/songs/peacheyes.mp3",
+    cover: "/covers/peacheyes.png",
+  },
+  {
+    title: "endlessly",
+    artist: "Bixby",
+    src: "/songs/endlessly.mp3",
+    cover: "/covers/endlessly.jpg",
+  },
+  {
+    title: "Out of Time",
+    artist: "The Weeknd",
+    src: "/songs/outoftime.mp3",
+    cover: "/covers/outoftime.jpg",
+  },
 ];
-
